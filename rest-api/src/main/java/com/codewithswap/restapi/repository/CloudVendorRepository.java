8fabeb73b8e7de9e568dafde24b25fcbc3c68284
swapnilpatalpure <swapnil24patalpure@gmai>
2024-04-05 01:04:54 +0530

    EXCEPTION HANDLING

--- rest-api/src/main/java/com/codewithswap/restapi/repository/CloudVendorRepository.java
+++ rest-api/src/main/java/com/codewithswap/restapi/repository/CloudVendorRepository.java
@@ -1,5 +1,7 @@
 package com.codewithswap.restapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.codewithswap.restapi.model.CloudVendor;
 
 @Repository
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+	
+	List<CloudVendor> findByVendorName(String vendorName);
 
 }
